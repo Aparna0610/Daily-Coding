a816579f3f7004aaf80830d932a9140c8828bc5a
Aparna Satheesh <55470055+Aparna0610@users.noreply.github.com>
2021-04-05 12:14:34 -0400

    Delete 4MadLibs.py

--- 4MadLibs.py
+++
@@ -1,7 +0,0 @@
-color = input("\nEnter a color: ")
-plural_noun = input("\nEnter a plural noun: ")
-celebrity = input("\nEnter a celebrity: ")
-
-print("\nRoses are " + color)
-print(plural_noun + " are blue")
-print("I love " + celebrity)
